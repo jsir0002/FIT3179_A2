2fc277069c7673f7ae99078bcad5c163ff004a80
jsir0002 <jsir0002@student.monash.edu>
2025-10-18 20:33:17 +1100

    Finished base graphs for the assignment.

--- vegaembed.js
+++ vegaembed.js
@@ -1,7 +1,19 @@
 var vg_1 = "avgtemp_aus.json";
 var vg_2 = "totalfirearea.json";
+var vg_3 = "drought.json";
+var vg_4 = "bananas.json";
+var vg_5 = "livestock.json";
+var vg_6 = "sunburn.json";
 
 vegaEmbed("#avgtemp_aus", vg_1).then(function(result) {
 }).catch(console.error);
 vegaEmbed("#totalfirearea", vg_2).then(function(result) {
+}).catch(console.error);
+vegaEmbed("#drought", vg_3).then(function(result) {
+}).catch(console.error);
+vegaEmbed("#bananas", vg_4).then(function(result) {
+}).catch(console.error);
+vegaEmbed("#livestock", vg_5).then(function(result) {
+}).catch(console.error);
+vegaEmbed("#sunburn", vg_6).then(function(result) {
 }).catch(console.error);
